93af6ffaee8fc26aafb7ba99710aa48ca5abc906
Racquel Munoz <racquelg85@hotmail.com>
2022-11-28 09:48:01 -0600

    added portfolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Nav from "./components/Nav";
+import About from "./components/About";
+import Gallery from "./components/Gallery";
+import ContactForm from "./components/Contact";
 
 function App() {
+  const [categories] = useState([
+    {
+      name: "react",
+      description: "photo-port application of a photographers work",
+    },
+    {
+      name: "my-sql",
+      description:
+        "This application is for a manager of a retail company who needs a back-end for an e-commerce website",
+    },
+    {
+      name: "NoSQL",
+      description: "NoSQL database with MongoDB called social-network-api",
+    },
+    {
+      name: "MVC",
+      description:
+        "NICHE application. Group project instututing Model-View-Controller software design pattern",
+    },
+    {
+      name: "PWA",
+      description: " text-editor-pwa institutes a progressive web application.",
+    },
+    {
+      name: "express",
+      description:
+        "The note-taker is an application that incorporates the node.js web application framework",
+    },
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+  const [contactSelected, setContactSelected] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      ></Nav>
+      <main>
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+        )}
+      </main>
     </div>
   );
 }
